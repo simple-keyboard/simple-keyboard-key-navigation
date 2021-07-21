94469572ee043638c6dfeffd4339ebadbdd6a5f1
Francisco Hodge <hello@franciscohodge.com>
2021-07-21 07:07:30 +0000

    Build update

--- build/index.js
+++ build/index.js
@@ -1,6 +1,6 @@
 /*!
  * 
- *   simple-keyboard-key-navigation v2.3.252
+ *   simple-keyboard-key-navigation v2.3.253
  *   https://github.com/hodgef/simple-keyboard-key-navigation
  * 
  *   Copyright (c) Francisco Hodge (https://github.com/hodgef)
